07a4924dcae4100674dbbf60209645a26783979d
aPaulTaylor <paul.a.taylor@gmail.com>
2020-12-06 17:22:09 +0000

    day 6 solution

---
+++ day6.py
@@ -0,0 +1,8 @@
+with open('c:/Users/paula/PycharmProjects/advent-of-code/day6-input.txt', 'r') as f:
+    resps_list = [x.strip().split('\n') for x in f.read().split('\n\n')]
+
+# Part 1
+print(sum(len(set(''.join(resps))) for resps in resps_list))
+
+# Part 2
+print(sum(sum(all(q in x for x in resps) for q in set(''.join(resps))) for resps in resps_list))
