78dffd82ccb8f3a1d9ec4ac37b9922f96f368fdd
aPaulTaylor <paul.a.taylor@gmail.com>
2020-12-10 17:13:27 +0000

    explanation of method for pt2

--- day10.py
+++ day10.py
@@ -25,6 +25,14 @@ def runs_of_1s(diffs):
 
 rl=runs_of_1s(diffs)
 
+# there are only joltage differences of 1 and 3 in the sequence.
+# a 3-jolt difference means both adapters must be used.
+# so the number of combinations depends on the lengths of runs of 1-jolt differences:
+# run of one 1-jolt diff: 1 combination (eg 2,5,6,9 -- all must be used)
+# run of two 1-jolt diffs: 2 combinations (eg 2,5,6,7,10 -- 6 can be used or not) - none of these in sequence so omitted
+# run of three 1-jolt diffs: 4 combinations (eg 2,5,6,7,8,11 -- 6 and 7 can each be used or not)
+# run of four 1-jolt diffs: 7 combinations (eg 2,5,6,7,8,9,12 -- any combination of at least one of 6,7,8)
+# extra *2 for the choice of either of the first two adapters (joltages 1,2) not covered by the above
 print(2**sum(r==2 for r in rl) * 4**sum(r==3 for r in rl) * 7**sum(r==4 for r in rl) * 2)
 
 
