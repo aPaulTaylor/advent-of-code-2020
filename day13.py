7d1d91638c5ccb3668799728d6189f92d83a515a
aPaulTaylor <paul.a.taylor@gmail.com>
2020-12-13 13:27:53 +0000

    day 13 cleanup

--- day13.py
+++ day13.py
@@ -11,8 +11,7 @@ while not timefound:
     dep_time+=1
     if any(dep_time % bus_no == 0 for bus_no in buses):
         timefound=True
-        print(dep_time - start_time)
-        print([bus for bus in buses if dep_time % bus == 0])
+        print((dep_time - start_time)*[bus for bus in buses if dep_time % bus == 0][0])
 
 # Part 2
 rel_times = []
@@ -22,7 +21,6 @@ for i,b in enumerate(busnotes[1].split(',')):
 
 
 def find_earliest_time(buses, rel_times):
-    print(buses,rel_times)
     if len(buses) == 1:
         return rel_times[0]
     time=rel_times[0] - buses[0]
@@ -31,12 +29,6 @@ def find_earliest_time(buses, rel_times):
         time += buses[0]
         if time % buses[1] == rel_times[1]:
             sol_found=True
-            print(time)
             return find_earliest_time([buses[0]*buses[1]]+buses[2:], [time]+rel_times[2:])
 
-
-test_buses = [7,13,59,31,19]
-test_times = [0,1,4,6,7]
-print(find_earliest_time(test_buses, [test_buses[i]-test_times[i] for i in range(len(test_buses))]))
-
-print(find_earliest_time(buses, [buses[i]-rel_times[i] for i in range(len(buses))]))
+print(find_earliest_time(buses, [-rel_times[i] % buses[i] for i in range(len(buses))]))
