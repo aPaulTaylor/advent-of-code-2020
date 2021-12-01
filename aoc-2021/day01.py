d1616306409d5470f88413dc35a4a9ccfd122f37
aPaulTaylor <paul.a.taylor@gmail.com>
2021-12-01 19:20:59 +0000

    fix function

--- aoc-2021/day01.py
+++ aoc-2021/day01.py
@@ -2,7 +2,7 @@ with open('input/day01-input.txt', 'r') as f:
     input = [int(x) for x in f.readlines()]
 
 def count_increases(seq,offset):
-    print( sum(seq[i+offset]>seq[i] for i in range(len(seq)-offset)) )
+    return sum(seq[i+offset]>seq[i] for i in range(len(seq)-offset))
 
 print(count_increases(input,1))
 print(count_increases(input,3))
