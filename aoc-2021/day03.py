64f66049d3dbd9a8560667766dee21964cdb0903
aPaulTaylor <paul.a.taylor@gmail.com>
2021-12-03 19:50:25 +0000

    shorten more

--- aoc-2021/day03.py
+++ aoc-2021/day03.py
@@ -2,14 +2,12 @@ with open('input/day03-input.txt', 'r') as f:
     input = [x.strip() for x in f.readlines()]
 
 ## Part 1
-majorities = [ '1' if sum(num[i]=='1' for num in input)*2>len(input) else '0' for i in range(12) ]
-gamma = int(''.join(majorities),2)
+gamma = int(''.join([ '1' if sum(num[i]=='1' for num in input)*2>len(input) else '0' for i in range(12) ]),2)
 print(gamma*(2**12 - gamma - 1))
 
 ## Part 2
 def reduce_diagnostic(diag,d,criteria):
-    num_ones = sum(x[d]=='1' for x in diag)
-    majority = '1' if num_ones*2 >= len(diag) else '0'
+    majority = '1' if sum(x[d]=='1' for x in diag)*2 >= len(diag) else '0'
     return [x for x in diag if (x[d]==majority)^(criteria=='CO2') ]
 
 prod=1
