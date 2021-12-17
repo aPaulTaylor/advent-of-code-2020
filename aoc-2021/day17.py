a9dfe3cceaa7bdaa3f68cffaf54f1c33e9073384
aPaulTaylor <paul.a.taylor@gmail.com>
2021-12-17 19:51:20 +0000

    day 17 shorten

--- aoc-2021/day17.py
+++ aoc-2021/day17.py
@@ -1,16 +1,13 @@
-#target area: x=209..238, y=-86..-59
+# target area: x=209..238, y=-86..-59
 
-def test_shot(vx,vy):
-    x=0
-    y=0
+def test_shot(v):
+    pos = [0, 0]
     while True:
-       x+=vx
-       y+=vy
-       if x> 238 or y < -86:
-           return False
-       if x >= 209 and y<= -59:
-           return True
-       vx = vx-1 if vx>0 else 0
-       vy = vy-1
+        pos = [pos[0] + v[0], pos[1] + v[1]]
+        if pos[0] > 238 or pos[1] < -86:
+            return False
+        if pos[0] >= 209 and pos[1] <= -59:
+            return True
+        v = [(v[0] - 1 if v[0] > 0 else 0), v[1] - 1]
 
-print(sum(sum(test_shot(vx,vy) for vx in range(20,239)) for vy in range(-86,86)))
+print(sum(sum(test_shot([vx, vy]) for vx in range(20, 239)) for vy in range(-86, 86)))
