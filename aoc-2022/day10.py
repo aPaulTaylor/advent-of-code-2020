1caf193407ee4aa922ac415aac91ee7d13163661
aPaulTaylor <paul.a.taylor@gmail.com>
2022-12-10 14:43:43 +0000

    day 10 more logical and tidied

--- aoc-2022/day10.py
+++ aoc-2022/day10.py
@@ -1,21 +1,21 @@
 with open('input/day10-input.txt', 'r') as f:
     program = [x.strip().split(' ') for x in f.readlines()]
 
-cur_cycle=0
-X=1
-X_sum=0
+cur_cycle = 1
+X = 1
+X_sum = 0
+display = ''
 
-display = '█' if abs(X-(cur_cycle % 40)) < 2 else ' '
 for instr in program:
-    cur_cycle+=1
-    X_sum+= (X*cur_cycle) if cur_cycle in [20,60,100,140,180,220] else 0
-    display = display + ('█' if abs(X-(cur_cycle % 40)) < 2 else ' ')
-    if instr[0]=='addx':
-        X_sum += (X * (cur_cycle+1)) if cur_cycle+1 in [20, 60, 100, 140, 180, 220] else 0
+    X_sum += (X * cur_cycle) if cur_cycle in [20, 60, 100, 140, 180, 220] else 0
+    display = display + ('█' if abs(X - ((cur_cycle - 1) % 40)) < 2 else ' ')
+    cur_cycle += 1
+    if instr[0] == 'addx':
+        X_sum += (X * cur_cycle) if cur_cycle in [20, 60, 100, 140, 180, 220] else 0
+        display = display + ('█' if abs(X - ((cur_cycle - 1) % 40)) < 2 else ' ')
         X += int(instr[1])
         cur_cycle += 1
-        display = display + ('█' if abs(X - (cur_cycle % 40)) < 2 else ' ')
 
 print(X_sum)
-for i in range(0,len(display),40):
-    print(display[i:i+40])
+for i in range(0, len(display), 40):
+    print(display[i:i + 40])
