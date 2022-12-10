f020919ff91a7f5cf4915142d74a5d34fe091ed6
aPaulTaylor <paul.a.taylor@gmail.com>
2022-12-10 14:28:57 +0000

    day 10 condensed

--- aoc-2022/day10.py
+++ aoc-2022/day10.py
@@ -3,32 +3,20 @@ with open('input/day10-input.txt', 'r') as f:
 
 cur_cycle=0
 X=1
-X_sum=0
-
-for instr in program:
-    cur_cycle+=1
-    if cur_cycle in [20,60,100,140,180,220]:
-        X_sum+=(X*cur_cycle)
-    if instr[0]=='addx':
-        cur_cycle += 1
-        if cur_cycle in [20, 60, 100, 140, 180, 220]:
-            X_sum += (X*cur_cycle)
-        X+=int(instr[1])
-
-print(X_sum)
-
-cur_cycle=0
-X=1
 display=''
+X_sum=0
 
 display = display + ('█' if abs(X-(cur_cycle % 40)) < 2 else ' ')
 for instr in program:
     cur_cycle+=1
+    X_sum+= (X*cur_cycle) if cur_cycle in [20,60,100,140,180,220] else 0
     display = display + ('█' if abs(X-(cur_cycle % 40)) < 2 else ' ')
     if instr[0]=='addx':
+        X_sum += (X * (cur_cycle+1)) if cur_cycle+1 in [20, 60, 100, 140, 180, 220] else 0
         X += int(instr[1])
         cur_cycle += 1
         display = display + ('█' if abs(X - (cur_cycle % 40)) < 2 else ' ')
 
+print(X_sum)
 for i in range(0,len(display),40):
     print(display[i:i+40])
