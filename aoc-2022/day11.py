f492302be3e564b1358d09ae9c0ad87d6dff9112
aPaulTaylor <paul.a.taylor@gmail.com>
2022-12-11 22:02:41 +0000

    day 11 combine parts

--- aoc-2022/day11.py
+++ aoc-2022/day11.py
@@ -10,32 +10,19 @@ destinations=[
 munctions=[lambda x: x*17, lambda x: x+1, lambda x: x+3, lambda x: x+5, lambda x: x*x, lambda x: x+2, lambda x: x+4, lambda x: x*19]
 inspect_counts=[0]*8
 
-for rnd in range(20):
-    for mnk in range(8):
-        for itm in items[mnk]:
-            inspect_counts[mnk]+=1
-            itm = munctions[mnk](itm)
-            itm = int(itm/3)
-            items[destinations[itm % div_tests[mnk]==0][mnk]].append(itm)
-        items[mnk]=[]
-
-print(sorted(inspect_counts)[-1]*sorted(inspect_counts)[-2])
-
 prod=1
 for d in div_tests:
     prod = prod*d
 
-items=[[int(x) for x in l.split(':')[1].split(',')] for l in monkey_behaviour[1::7]]
-inspect_counts=[0]*8
-
-for rnd in range(10000):
-    for mnk in range(8):
-        for itm in items[mnk]:
-            inspect_counts[mnk]+=1
-            itm = munctions[mnk](itm) % prod
-            items[destinations[itm % div_tests[mnk]==0][mnk]].append(itm)
-        items[mnk]=[]
-
-print(sorted(inspect_counts)[-1]*sorted(inspect_counts)[-2])
+for run_params in [(20,lambda x: int(x/3)),(10000,lambda x: x % prod)]:
+    for rnd in range(run_params[0]):
+        for mnk in range(8):
+            for itm in items[mnk]:
+                inspect_counts[mnk]+=1
+                itm = munctions[mnk](itm) % prod
+                itm = run_params[1](itm)
+                items[destinations[itm % div_tests[mnk]==0][mnk]].append(itm)
+            items[mnk]=[]
+    print(sorted(inspect_counts)[-1]*sorted(inspect_counts)[-2])
 
 
